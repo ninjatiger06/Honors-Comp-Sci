34f06ca580b52a49be84ac4f7e85d767ffe1e4e0
ninja_tiger06 <jpfefferman@gmail.com>
2022-09-01 11:46:37 -0700

    More work on Lab 10, completed worksheet

--- week10/sentiment.py
+++ week10/sentiment.py
@@ -4,24 +4,62 @@
 	Description: Uses reviews from movieReviews.txt to associate the connotation
 				 of a word with a certain review score
 """
-def readFile():
+
+from searches import *
+
+def readStopWords():
 	"""
-		Purpose: Reads in the file and calls other functions to look at the lines
+		Purpose: reads in the stop words file
+		Parameters: None
+		Returns: A List of all the stop words
+	"""
+	stopWordsFile = open("stopwords.txt", 'r')
+	stopWordsLst = []
+	for line in stopWordsFile:
+		stopWordsLst.append(line)
+
+	return stopWordsLst
+
+def readReviews():
+	"""
+		Purpose: Reads in the reviews file and calls other functions to look at the lines
 		Parameters: None
 		Returns: None
 	"""
-	reviewsFile = open('movieReviews.txt', 'r')
-	L = []
-	for line in reviewFile:
+	reviewsFile = open('smallReviews.txt', 'r')
+	allReviews = []
+
+	# read in each review
+	for line in reviewsFile:
+
+		# clean up the review
 		line = line.strip().lower()
 		lineLst = line.split()
-		score = int(lineLst[0]) - 2
-		review = lineLst[1:]
-		for word in review:
-			if word not in
 
+		# get and alter the score
+		score = int(lineLst[0]) - 2
+
+		# get the rest of the words in the review
+		review = lineLst[1:]
+
+		# read in the stop words
+		stopWords = readStopWords()
+
+		for word in review:
+			# get rid of non-alpha characters
+			if word.isalpha() == False:
+				review.remove(word)
+
+			# if the word is a stop words, remove it from the review
+			if binarySearch(word, stopWords):
+				review.remove(word)
+			else:
+				if word not in allReviews:
+					allReviews.append([score, word])
+
+	print(allReviews)
 def main():
-	readFile()
+	readReviews()
 
 
 main()
