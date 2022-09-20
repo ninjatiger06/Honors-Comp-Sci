fa23a18b4c5c67e51cb661157b5adc00551c20af
ninja_tiger06 <jpfefferman@gmail.com>
2022-09-20 12:55:07 -0700

    More lab work

--- week11/lab11/cleanstring.py
+++ week11/lab11/cleanstring.py
@@ -4,6 +4,20 @@ Description: This program recursively searches through a user-inputted string
 Date: 9/19/22
 Author: Jonas Pfefferman '24
 """
+def removeChar(str, ch):
+	"""
+		Description: Takes in a string and removes the first instance of a
+					 specific character
+		Parameters: String to clean (str) and character to remove (str)
+		Returns: The string with the first instance of the character removed
+	"""
+
+	if len(str) <= 1:
+		return str
+	else:
+		# if the first character is the one to remove, its value will become 0 (false)
+		return str[0] * (str[0] != ch) + removeChar(str[1:], ch)
+
 
 def main():
 
@@ -12,6 +26,8 @@ def main():
 	while len(ch) != 1:
 		print("\nInvalid. Please enter a single character")
 		ch = input("ch: ")
+
+	print(removeChar(str, ch))
 
 
 main()
