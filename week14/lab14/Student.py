7be1c43be899803de37ed02f35f5c1eadd3b22de
ninja_tiger06 <jpfefferman@gmail.com>
2022-11-07 12:58:30 -0800

    Started on lab14

---
+++ week14/lab14/Student.py
@@ -0,0 +1,7 @@
+class Student(object):
+	""" class for single student object"""
+
+	def __init__(self, firstname, lastname):
+		"""constructor for student object given student's first and last names (strings)"""
+		self.firstname = firstname
+		self.lastname = lastname
