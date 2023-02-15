b6ba83c72a694d9c048736b9bf53a6effa00e943
ninja_tiger06 <jpfefferman@gmail.com>
2023-02-15 09:28:22 -0800

    Finished basic version of reading

--- week16/lab16/reading.cpp
+++ week16/lab16/reading.cpp
@@ -16,5 +16,24 @@ int main() {
     int pagesReadTotal;
     int pagesRead;
 
+    cout << "Pages to read? ";
+    cin >> totalPages;
+    pagesLeft = totalPages;
+    cout << "Days left? ";
+    cin >> daysLeft;
+
+    pagesReadTotal = 0;
+    for (int i=0; i <= daysLeft + 1; i++) {
+        cout << "Pages read: ";
+        cin >> pagesRead;
+
+        daysLeft -= 1;
+        pagesReadTotal += pagesRead;
+        pagesLeft = totalPages - pagesReadTotal;
+
+        cout << "You have read " << pagesReadTotal << " out of " << totalPages << " pages." << endl;
+        cout << pagesLeft << " pages left, " << daysLeft << " days to go." << endl;
+    }
+
     return 0;
 }
